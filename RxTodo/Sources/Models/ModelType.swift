c3a1a5dfc36b13acdb6651dd901e3a4bd1c39ea1
sogihyeon <william.so@gatda.com>
2020-05-23 18:47:49 +0900

    add comment

--- RxTodo/Sources/Models/ModelType.swift
+++ RxTodo/Sources/Models/ModelType.swift
@@ -7,6 +7,7 @@
 //
 
 import Then
+import Foundation
 
 protocol Identifiable {
   associatedtype Identifier: Equatable
@@ -22,4 +23,22 @@ extension Collection where Self.Iterator.Element: Identifiable {
     return self.index { $0.id == element.id }
   }
 
+}
+
+/*
+ - associatedtype :
+    - type 알리아스랑 똑같지만 프로토콜에서 쓰인다
+    - Equatable이랑 똑같고 이름만 다른 Identifier라는 타입을 정의 :line 12
+ - Equatable :
+    - == 연산자를 사용할 수 있는 타입 표준 라이브러리 대부분 이 타입을 따른다
+    - 프로토콜 타입임
+ -
+ 
+ */
+
+protocol Container {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
 }
